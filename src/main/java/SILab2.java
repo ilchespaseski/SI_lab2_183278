889cee220b3b18ff2a9681c79574b907843e2681
Ilche Spaseski <ile.spasesky@gmail.com>
2020-08-25 19:00:58 +0200

    first commit

--- src/main/java/SILab2.java
+++ src/main/java/SILab2.java
@@ -1,48 +1,20 @@
 import java.util.List;
 
-class User {
-    String username;
-    String password;
-    String email;
-
-    public User(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-}
-
-
-public class SILab2 {
-
-    public boolean function (User user, List<String> allUsers) {
-        if (user!=null) {
-            if (user.getUsername()!=null && user.getEmail()!=null && !allUsers.contains(user.getUsername())) {
-                boolean atChar = false, dotChar =   false;
-                for (int i=0;i<user.getEmail().length();i++) {
-                    if (user.getEmail().charAt(i)=='@')
-                        atChar = true;
-                    if (atChar && user.getEmail().charAt(i)=='.') {
-                        dotChar = true;
-                    }
-                }
-                if (atChar && dotChar) {
+public class PrimeExample {
+    static boolean checkPrime(int n) {
+        if (n < 0) {
+            throw new RuntimeException("Number should not be negative");
+        }
+        int m = n / 2;
+        if (n == 0 || n == 1) {
+            return false;
+        } else {
+            for (int i = 2; i <= m; i++) {
+                if (n % i == 0) {
                     return true;
                 }
             }
+            return false;
         }
-        return false;
     }
 }
